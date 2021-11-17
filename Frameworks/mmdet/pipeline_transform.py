10ba38b33c8cf849c216ecf2e5fc194f9665d2e0
KobryLee <37265633+KobryLee@users.noreply.github.com>
2021-11-17 11:31:52 +0800

    Create pipeline_transform.py

---
+++ Frameworks/mmdet/pipeline_transform.py
@@ -0,0 +1,2 @@
+# transform det bboxes under a data aug process to another data aug process
+# aug process contain: flip, resize and so on, transform the size and position to a specific size and position
