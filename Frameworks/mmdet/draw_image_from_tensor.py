f4d964c972365db9b395c66000a6b30bf9f74f43
KobryLee <37265633+KobryLee@users.noreply.github.com>
2021-11-30 15:54:40 +0800

    Update draw_image_from_tensor.py

--- Frameworks/mmdet/draw_image_from_tensor.py
+++ Frameworks/mmdet/draw_image_from_tensor.py
@@ -6,13 +6,15 @@
 
 from PIL import Image
 import matplotlib.pyplot as plt
-img=data['img'].data + torch.tensor([103.530, 116.280, 123.675]).reshape(-1,1,1)
-
+img=weak['img'].data + torch.tensor([103.530, 116.280, 123.675]).reshape(-1,1,1)
 # to (H,W,3)
-
 img=torch.transpose(img,2,0)
 img=torch.transpose(img,1,0)
 img_np = np.uint8(img.numpy())
 img_np = cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
-image=Image.fromarray(img_np)
+cv2.rectangle(img_np,(0,0),         #(x1,y1)
+              (100,200),            #(x2,y2)
+              (0,255,0),2)
+image=Image.fromarray(img_np)      
 plt.imshow(image)
+
